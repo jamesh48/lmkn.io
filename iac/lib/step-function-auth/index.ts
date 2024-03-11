5c11e45ca0aaba1ae69954209d6063ac73cab88f
jamesh48 <jameshrivnak4@gmail.com>
2024-03-11 12:02:59 -0600

    does this work

--- iac/lib/step-function-auth/index.ts
+++ iac/lib/step-function-auth/index.ts
@@ -29,12 +29,12 @@ export class StepFunctionsAuthFlow extends Construct {
     super(scope, id);
 
     console.log('path');
-    console.log(path.resolve('./'));
+    console.log(path.resolve('../'));
     this.sendCodeLambda = new lambda.Function(this, 'lmk-send-code-lambda', {
       functionName: 'lmk-send-code-lambda',
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset('backend/dist/sendCode'),
+      code: lambda.Code.fromAsset('../backend/dist/sendCode'),
       role: props.iamRole,
       environment: {
         SMS_APPLICATION_ID: props.env.SMS_APPLICATION_ID,
